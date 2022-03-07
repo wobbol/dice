dfd9a97ebf78dc629103a41ee1e52e24630f1dc0
Nicole Fontenot <nfontenot27@gmail.com>
2022-03-07 10:40:28 -0600

    Make diceroll_init() idempotent

--- diceroll/rand_dev_urandom.c
+++ diceroll/rand_dev_urandom.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-static FILE *rand_f;
+static FILE *rand_f = NULL;
 
 void init_random(void)
 {
+	if(rand_f){
+		return;
+	}
 	rand_f = fopen("/dev/urandom","r");
 	if(!rand_f){
 		perror("Cannot open /dev/urandom");
